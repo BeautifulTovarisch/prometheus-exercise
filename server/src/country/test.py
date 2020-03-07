76cd420f8438adcae2874b478f30ebce14555624
Anthony Re <anthonyjosephre@gmail.com>
2020-03-07 10:46:17 -0500

    added docstring for country module; fixed broken test, wrote test to fetch countries by region

--- server/src/country/test.py
+++ server/src/country/test.py
@@ -5,22 +5,41 @@ from unittest import TestCase
 from country.model import (
     Country,
 
-    fetch_regions
+    fetch_regions,
+    fetch_countries
 )
 
 from database.mod import create_db_engine
 
 class TestCountry(TestCase):
     def test_fetch_regions(self):
-        expected = ['Eastern Asia',
-                    'Southern and Central Asia',
-                    'Southeast Asia',
-                    'Middle East']
+        expected = sorted(['Middle East',
+                           'Eastern Asia',
+                           'Southeast Asia',
+                           'Southern and Central Asia'])
 
         conn = create_db_engine().connect()
 
-        regions = [country['region'] for country in fetch_regions(conn, 'Asia')]
+        regions = sorted([country['region'] for country in fetch_regions(conn, 'Asia')])
 
         self.assertEqual(regions, expected)
-        
+
         conn.close()
+
+    def test_fetch_counties(self):
+        expected = sorted(['Hong Kong',
+                           'Japan',
+                           'China',
+                           'North Korea',
+                           'South Korea',
+                           'Macao',
+                           'Mongolia',
+                           'Taiwan'])
+
+        conn = create_db_engine().connect()
+
+        countries = sorted([country['name'] for country in fetch_countries(conn, 'Eastern Asia')])
+
+
+
+        self.assertEqual(countries, expected)
